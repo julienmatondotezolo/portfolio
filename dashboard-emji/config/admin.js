6e3e223441b2f78bed740e64134474749d7d0986
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-12-21 04:23:07 +0100

    restart strapi

--- dashboard-emji/config/admin.js
+++ dashboard-emji/config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'b5a7158fbc0bbf4b5b73fc359338fe65'),
+    secret: env('ADMIN_JWT_SECRET', 'ebf24e46b2d937c1d011595da92fa9d6'),
   },
 });
