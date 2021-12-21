b47542c73eabf7ab4af70b41721ad6c2e88eab1a
Julien Matondo <Julien.matondo-tezolo@student.ehb.be>
2021-12-21 03:24:09 +0100

    STARTING STRAPI

--- dashboard-emji/config/database.js
+++ dashboard-emji/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-176-34-105-15.eu-west-1.compute.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'de81b8f7n99spf'),
-      user: env('DATABASE_USERNAME', 'quuyrzlyjzyrez'),
-      password: env('DATABASE_PASSWORD', '35fa169a633e9f3fcdbe0274af4d640aa060634ea68e3df958828dd5a4a64b7c'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
