f9dcc2d896fb4c7bbb3b09daf82a3d0ee6f51538
Julien Matondo <julien.matondo-tezolo@bothive.be>
2022-10-03 19:33:15 +0200

    fix image preview on dashboard

--- dashboard-emji/config/middlewares.js
+++ dashboard-emji/config/middlewares.js
@@ -13,21 +13,21 @@
 module.exports = [
   // ...
   'strapi::errors',
-  'strapi::security',
-  // {
-  //   name: 'strapi::security',
-  //   config: {
-  //     contentSecurityPolicy: {
-  //       useDefaults: true,
-  //       directives: {
-  //         'connect-src': ["'self'", 'https:'],
-  //         'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-  //         'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-  //         upgradeInsecureRequests: null,
-  //       },
-  //     },
-  //   },
-  // },
+  // 'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::query',
   'strapi::body',
